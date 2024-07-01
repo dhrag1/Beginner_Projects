391c97e344be5e49083dd1dadba31403714eaa55
dhrag1 <mail2dhruv101@gmail.com>
2024-07-01 13:14:54 -0400

    Added number guessing game

---
+++ guess_the_number.py
@@ -0,0 +1,29 @@
+import random
+
+
+min = int(input("Enter an integer value for the minimum potential guess number: "))
+max  = int(input("Enter an integer value for the maximum potential guess number: "))
+guess_num = 1
+
+guess = int()
+
+while True:
+    target = random.randint(min, max)
+
+    if target != 0:
+        break
+
+
+print("The target number has been randomly selected.")
+guess = int(input("Enter your guess: "))
+
+while guess != target:
+    if guess > target:
+        print("Try Again! Your guess was too high!")
+    elif guess < target:
+        print("Try Again! Your guess was too low!")
+    guess_num += 1
+    guess = int(input("Enter your guess: "))
+
+print("You successfully guessed the number in", guess_num, "guesses!")
+        
